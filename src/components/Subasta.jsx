996a864803d82fb65aabfaf015e5f4b29c2f0e38
Nicocaci <nicko.caci@gmail.com>
2025-04-03 15:46:24 -0300

    push

--- src/components/Subasta.jsx
+++ src/components/Subasta.jsx
@@ -20,12 +20,26 @@ const Subasta = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
+        // Mostrar el mensaje de alerta con toast
+        toast.info("Bienvenido a AutoAPP\n\n" +
+            "• La oferta ganadora deberá ser abonada dentro de los 7 días del cierre de la subasta.\n\n" +
+            "• Una vez abonada, se podrá retirar la unidad y las cédulas.\n" +
+            "• Al vender el rodado, se deberá enviar fotocopias del DNI del/los futuro/s titular/es.\n\n" +
+            "• Si el vehículo no es vendido dentro de los noventa días subastado, se procederá a transferir el mismo al ganador de la subasta.", {
+            position: "top-center", // Puedes cambiar la posición
+            autoClose: 3000, // Duración en milisegundos
+            hideProgressBar: true, // Ocultar barra de progreso
+            closeOnClick: true,
+            className: "toast-custom"  // Cerrar al hacer click
+        });
+    }, []);
+    useEffect(() => {
         const fetchSubastas = async () => {
             try {
                 const response = await axios.get(
                     "https://martelli-automotes-back-production.up.railway.app/api/subasta"
                     //"http://localhost:3000/api/subasta"
-                    );
+                );
                 if (Array.isArray(response.data)) {
                     setSubasta(response.data);
                     fetchHighestBids(response.data);
