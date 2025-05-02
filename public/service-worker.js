67691e4a0adc1391a648bd8d6b63d98286258957
Nicocaci <nicko.caci@gmail.com>
2025-05-02 10:48:03 -0300

    push

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,13 @@
 self.addEventListener('install', (event) => {
-    console.log('Service Worker instalado');
-  });
+  console.log('Service Worker instalado');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activado');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Esto permite interceptar peticiones y ayuda a cumplir con los requisitos mínimos de una PWA
+  event.respondWith(fetch(event.request));
+});
