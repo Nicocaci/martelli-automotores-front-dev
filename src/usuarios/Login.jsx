56b9399b193396ffdabe697ac4282d5cdb689bcf
Nicolás Caci <nicko.caci@gmail.com>
2026-05-21 20:05:43 -0300

    push

--- src/usuarios/Login.jsx
+++ src/usuarios/Login.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import '../css/Login.css';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import Swal from 'sweetalert2';
+import React from "react";
+import "../css/Login.css";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const Login = () => {
@@ -16,15 +16,21 @@ const Login = () => {
   const [deferredPrompt, setDeferredPrompt] = useState(null);
   const navigate = useNavigate();
 
-  // VERIFICAMOS TOKEN
-
   useEffect(() => {
-    const token = Cookies.get('access_token');
-    if (token) {
-      navigate('/subasta');
-    }
-  }, [navigate]);
+    const verificarSesion = async () => {
+      try {
+        await axios.get(`${apiUrl}/usuarios/verify`, {
+          withCredentials: true,
+        });
 
+        navigate("/subasta");
+      } catch (error) {
+        console.log("Usuario no autenticado");
+      }
+    };
+
+    verificarSesion();
+  }, [navigate]);
   const handleLogin = async (e) => {
     e.preventDefault();
     if (!email || !password) {
@@ -32,38 +38,36 @@ const Login = () => {
       return;
     }
     try {
-      const normalizedEmail = email.trim().toLowerCase(); // ⬅️ normalización
+      const normalizedEmail = email.trim().toLowerCase();
 
       const response = await axios.post(
-        `${apiUrl}/usuarios/login`
-        ,
+        `${apiUrl}/usuarios/login`,
         { email: normalizedEmail, password },
-        { headers: { "Content-Type": "application/json" }, withCredentials: true }
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        },
       );
-      console.log("Headers recibidos:", response.headers); // 🔍 Ver cookies en la respuesta
-      console.log("Cookies actuales:", document.cookie); // 🔍
+
       Swal.fire({
         title: "¡Inicio de sesión exitoso!",
-        text: "Bienvenido a AutoSMart",
+        text: "Bienvenido a AutoSmart",
         icon: "success",
-        showConfirmButton: false, // Oculta el botón
-        timer: 2000,              // Duración de 2 segundos (2000 ms)
-        timerProgressBar: true    // Opcional: muestra una barra de progreso del tiempo
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
       }).then(() => {
-        navigate('/subasta'); // Navegamos después de que se cierre el alerta automáticamente
+        navigate("/subasta");
       });
     } catch (error) {
       if (error.response) {
         setError(error.response.data.message || "Error en el inicio de sesión");
-        // 🔥 Mostrar SweetAlert en caso de error
         Swal.fire({
           title: "Error",
           text: error.response.data.message || "Error en el inicio de sesión",
           icon: "error",
           confirmButtonText: "Intentar nuevamente",
-          customClass: {
-            confirmButton: "boton-swal-error" // Clase personalizada
-          }
+          customClass: { confirmButton: "boton-swal-error" },
         });
       } else {
         setError("Error de conexión con el servidor");
@@ -72,12 +76,10 @@ const Login = () => {
           text: "No se pudo conectar con el servidor",
           icon: "warning",
           confirmButtonText: "Reintentar",
-          customClass: {
-            confirmButton: "boton-swal-error" // Clase personalizada
-          }
+          customClass: { confirmButton: "boton-swal-error" },
         });
       }
-    };
+    }
   };
 
   useEffect(() => {
@@ -87,10 +89,13 @@ const Login = () => {
       setShowInstallButton(true);
     };
 
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
 
     return () => {
-      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt,
+      );
     };
   }, []);
 
@@ -98,10 +103,10 @@ const Login = () => {
     if (deferredPrompt) {
       deferredPrompt.prompt();
       const { outcome } = await deferredPrompt.userChoice;
-      if (outcome === 'accepted') {
-        console.log('El usuario aceptó la instalación');
+      if (outcome === "accepted") {
+        console.log("El usuario aceptó la instalación");
       } else {
-        console.log('El usuario rechazó la instalación');
+        console.log("El usuario rechazó la instalación");
       }
       setDeferredPrompt(null);
       setShowInstallButton(false);
@@ -109,25 +114,40 @@ const Login = () => {
   };
 
   return (
-    <div className='mainform'>
-      <form className='form' onSubmit={handleLogin}>
+    <div className="mainform">
+      <form className="form" onSubmit={handleLogin}>
         <h1>Login</h1>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
         <label htmlFor="">Email:</label>
-        <input type="text" name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label htmlFor="">Contraseña:</label>
-        <input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button className='' type='submit'>Iniciar sesión</button>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="" type="submit">
+          Iniciar sesión
+        </button>
         {showInstallButton && (
-          <button className='boton-menu' onClick={handleInstallClick}>Instalar App</button>
+          <button className="boton-menu" onClick={handleInstallClick}>
+            Instalar App
+          </button>
         )}
         <Link to="/signup">
-          <button className='' type="button">Registrarse</button>
+          <button className="" type="button">
+            Registrarse
+          </button>
         </Link>
-
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
